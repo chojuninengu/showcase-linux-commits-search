1606a3236fab8aa2a13281433b838f735f2db9d3
chojuninengu <chojuninengu@gmail.com>
2025-04-24 09:39:42 +0100

    Update Typesense configuration to use Parcel environment variables

--- src/app.js
+++ src/app.js
@@ -23,12 +23,12 @@ import STOP_WORDS from './utils/stop_words.json';
 import { history } from 'instantsearch.js/es/lib/routers';
 
 let TYPESENSE_SERVER_CONFIG = {
-  apiKey: process.env.TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
+  apiKey: process.env.PARCEL_TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
   nodes: [
     {
-      host: process.env.TYPESENSE_HOST,
-      port: process.env.TYPESENSE_PORT,
-      protocol: process.env.TYPESENSE_PROTOCOL,
+      host: process.env.PARCEL_TYPESENSE_HOST,
+      port: process.env.PARCEL_TYPESENSE_PORT,
+      protocol: process.env.PARCEL_TYPESENSE_PROTOCOL,
     },
   ],
   numRetries: 8,
